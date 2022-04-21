510a58057ee8225ce7336650a130bd24a3a667f3
hemanth <hemanthreddyp11@gmail.com>
2022-04-21 12:17:44 +0530

    Created the OrdersMicroservices and registered to the Eureka Server

---
+++ OrdersMicroservices/src/test/java/com/OrdersMicroservices/OrdersMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.OrdersMicroservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdersMicroservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
