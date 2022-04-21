85f787911be7621e55e4254d2ad84ec51c6a45f6
hemanth <hemanthreddyp11@gmail.com>
2022-04-21 12:20:47 +0530

    Created the ZuulgatewayMicroservices and registered to the Eureka Server

---
+++ ZuulgatewayServerMicroservices/src/test/java/com/ZuulgatewayServerMicroservices/ZuulgatewayServerMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ZuulgatewayServerMicroservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulgatewayServerMicroservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
