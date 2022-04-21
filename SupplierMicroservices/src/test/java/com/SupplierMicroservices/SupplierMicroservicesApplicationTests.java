521882ba4dcc5cec2f736c8f0b10c9c841a23e87
hemanth <hemanthreddyp11@gmail.com>
2022-04-21 12:20:09 +0530

    Created the SupplierMicroservices and registered to the Eureka Server

---
+++ SupplierMicroservices/src/test/java/com/SupplierMicroservices/SupplierMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SupplierMicroservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupplierMicroservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
