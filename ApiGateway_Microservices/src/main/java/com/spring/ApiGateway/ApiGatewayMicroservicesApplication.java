fdf5e71cc4c01db98ddee5b6fa5308b20de88365
hemanth <hemanthreddyp11@gmail.com>
2022-04-30 14:54:51 +0530

    Changed the Code

---
+++ ApiGateway_Microservices/src/main/java/com/spring/ApiGateway/ApiGatewayMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package com.spring.ApiGateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayMicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayMicroservicesApplication.class, args);
+	}
+
+}
